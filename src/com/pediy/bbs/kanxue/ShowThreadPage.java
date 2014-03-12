3f43e2e77881ce35cdacded6e98df7b858a3bf78
hho2002 <hho2002@gmail.com>
2014-03-12 18:24:03 +0800

    修正错误的JSON数据导致fastjson输出异常退出
    类似调用fastjson的地方有很多，可能会有相同的错误

--- src/com/pediy/bbs/kanxue/ShowThreadPage.java
+++ src/com/pediy/bbs/kanxue/ShowThreadPage.java
@@ -10,6 +10,7 @@ import org.apache.http.cookie.Cookie;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONException;
 import com.alibaba.fastjson.JSONObject;
 import com.pediy.bbs.kanxue.CustomTextView.LocalLinkMovementMethod;
 import com.pediy.bbs.kanxue.net.HttpClientUtil;
@@ -34,11 +35,13 @@ import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Looper;
 import android.os.Message;
 import android.text.Html;
 import android.text.Html.ImageGetter;
 import android.text.Spannable;
 import android.text.format.Time;
+import android.util.Log;
 //import android.text.Spanned;
 //import android.util.DisplayMetrics;
 //import android.view.Gravity;
@@ -72,9 +75,9 @@ public class ShowThreadPage extends Activity implements IXListViewListener, OnIt
 	private RefreshActionBtn m_refreshBtn;
 	private TextView m_titleView;
 	private long m_lastUpdateTime = 0;
-	
+
 	private ImageGetter m_imgGetter = new ImageGetter() {
-		@Override  
+		@Override
         public Drawable getDrawable(String source) {
 			URL url = null;
 			Bitmap bitmap = null;
@@ -85,7 +88,7 @@ public class ShowThreadPage extends Activity implements IXListViewListener, OnIt
 				e.printStackTrace();
 				return null;
 			}
-			
+
 			File file = ImageViewWithCache.getCachedImgFromUrl(url);
 			if (file != null) {
 				bitmap = BitmapFactory.decodeFile(file.getAbsolutePath());
@@ -101,9 +104,9 @@ public class ShowThreadPage extends Activity implements IXListViewListener, OnIt
 			Drawable drawable = new BitmapDrawable(getResources(), bitmap);
 			drawable.setBounds(0, 0, bitmap.getWidth(), bitmap.getHeight());
 			return drawable;
-        }  
+        }
 	};
-	
+
 	private Handler m_handler = new Handler() {
 		@Override
 		public void handleMessage(Message msg) {
@@ -111,7 +114,7 @@ public class ShowThreadPage extends Activity implements IXListViewListener, OnIt
 				m_pd.dismiss();
 			}
 			switch (msg.what) {
-			case HttpClientUtil.NET_SUCCESS:				
+			case HttpClientUtil.NET_SUCCESS:
 				m_adapter.notifyDataSetChanged();
 				//只在listview中有数据之后才启用pull load
 				if (m_currentPage == 2 && m_totalPage != 0) {
@@ -131,12 +134,12 @@ public class ShowThreadPage extends Activity implements IXListViewListener, OnIt
 			default:
 				break;
 			}
-			
+
 			if (m_refreshBtn.isRefreshing()) {
 				m_listView.setSelection(1);
 				m_refreshBtn.endRefresh();
 			}
-			
+
 			//停止上拉下拉时的processBar
 			if (m_listView.getPullLoading()) {
 				//加载到最后一页时禁用pull load
@@ -146,21 +149,21 @@ public class ShowThreadPage extends Activity implements IXListViewListener, OnIt
 					m_listView.stopLoadMore();
 				}
 			}
-			
+
 			if (m_listView.getPullRefreshing()) {
 				m_listView.stopRefresh();
 			}
-			
+
 			m_pBar.setVisibility(View.GONE);
 		}
 	};
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		this.setContentView(R.layout.show_thread_page);
-					
-		m_listView = (XListView)this.findViewById(R.id.showthreadListview);	
+
+		m_listView = (XListView)this.findViewById(R.id.showthreadListview);
 		m_listView.setPullLoadEnable(false);
 		m_listView.setPullRefreshEnable(true);
 		m_listView.setXListViewListener(this);
@@ -172,10 +175,10 @@ public class ShowThreadPage extends Activity implements IXListViewListener, OnIt
 		m_adapter = new ShowthreadAdapter();
 		m_listView.setAdapter(m_adapter);
 		m_listView.requestFocus();
-		
+
 		m_pBar = (ProgressBar)this.findViewById(R.id.showThreadProgressBar);
 		m_refreshBtn = (RefreshActionBtn)this.findViewById(R.id.showThreadRefreshBtn);
-		
+
 		Bundle data = this.getIntent().getExtras();
 		m_id = data.getInt("id");
 		if (data.getInt("open") == 0) {
@@ -183,7 +186,7 @@ public class ShowThreadPage extends Activity implements IXListViewListener, OnIt
 		}
 		loadModel(m_currentPage++);
 	}
-	
+
 	@Override
 	public void onRefresh() {
 		m_refreshBtn.startRefresh();
@@ -194,16 +197,16 @@ public class ShowThreadPage extends Activity implements IXListViewListener, OnIt
 	public void onLoadMore() {
 		loadModel(m_currentPage++);
 	}
-	
+
 	public void onBackBtnClick(View v) {
 		this.finish();
 	}
-	
+
 	public void onPageTitleClick(View v) {
 		//滑动listView到顶端
 		m_listView.setSelection(0);
 	}
-	
+
 	private void refresh() {
 		//刷新之前判断有无新贴
 		Api.getInstance().checkNewPostInShowThreadPage(m_id, m_lastUpdateTime, new NetClientCallback() {
@@ -223,7 +226,7 @@ public class ShowThreadPage extends Activity implements IXListViewListener, OnIt
 						public void run() {
 							Toast.makeText(ShowThreadPage.this, "无新贴", Toast.LENGTH_SHORT).show();
 						}
-						
+
 					});
 					m_handler.sendEmptyMessage(0);
 					return;
@@ -231,10 +234,10 @@ public class ShowThreadPage extends Activity implements IXListViewListener, OnIt
 				m_currentPage = 1;
 				loadModel(m_currentPage++);
 			}
-			
+
 		});
 	}
-	
+
 	public void onReplyBtnClick(View v) {
 		if (!Api.getInstance().isLogin()) {
 			this.startActivity(new Intent(this, LoginPage.class));
@@ -250,7 +253,7 @@ public class ShowThreadPage extends Activity implements IXListViewListener, OnIt
 			replyTextView.requestFocus();
 			return;
 		}
-		
+
 		this.m_pd = ProgressDialog.show(this, "提示", "回帖中，请稍后……", true, true);
 		Api.getInstance().quickReply(this.m_id, replyTextView.getText().toString(), new NetClientCallback() {
 
@@ -280,7 +283,7 @@ public class ShowThreadPage extends Activity implements IXListViewListener, OnIt
 						default:
 							break;
 						}
-						
+
 						JSONObject item = new JSONObject();
 						item.put("postid", -1);
 						item.put("thumbnail", 0);
@@ -288,37 +291,37 @@ public class ShowThreadPage extends Activity implements IXListViewListener, OnIt
 						item.put("userid", Api.getInstance().getLoginUserId());
 						item.put("avatar", Api.getInstance().getIsAvatar());
 						item.put("avatardateline", "");
-						
+
 						Time time = new Time();
 						time.setToNow();
-						
+
 						int hour12 = time.hour;
 						String hour = null;
 						if (time.hour > 12) {
 							hour12 = time.hour - 12;
-						} 
+						}
 						hour = "" + ((hour12 > 10)?hour12:("0"+hour12));
 						item.put("posttime", hour + ":" + time.minute + " " + ((time.hour > 12)?"PM":"AM"));
-						
+
 						item.put("postdate", "今天");
 						item.put("message", replyTextView.getText().toString());
 						m_model.add(item);
 						m_adapter.notifyDataSetChanged();
 						m_listView.setSelection(m_listView.getCount());
-						
+
 						replyTextView.setText("");
 						replyTextView.clearFocus();
 						//关闭软键盘
 						InputMethodManager imm = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
 						imm.hideSoftInputFromWindow(replyTextView.getWindowToken(), 0);
 					}
-					
+
 				});
 			}
-			
+
 		});
 	}
-	
+
 	private void safeRefresh() {
 		//如果开始网络传输失败了，强制重新刷新
 		if (this.m_model == null) {
@@ -328,7 +331,7 @@ public class ShowThreadPage extends Activity implements IXListViewListener, OnIt
 			refresh();
 		}
 	}
-	
+
 	public void onRefreshBtnClick(View v) {
 		if (m_refreshBtn.isRefreshing())
 			return;
@@ -337,22 +340,22 @@ public class ShowThreadPage extends Activity implements IXListViewListener, OnIt
 		m_listView.pullRefreshing();
 		safeRefresh();
 	}
-	
+
 	@Override
 	public void onItemClick(AdapterView<?> arg0, View v, int position, long id) {
 		//对header进行点击也会触发该事件
-		if (position < 2) 
+		if (position < 2)
 			return;
-		
+
 		final JSONObject item = m_model.getJSONObject(position-2);	//listView 中有两个header，所以此处减去2
-		if (item.getInteger("thumbnail") != 1) 
+		if (item.getInteger("thumbnail") != 1)
 			return;
-		
+
 		final ThreadItemFooter itemFooter = (ThreadItemFooter)v.findViewById(R.id.showthreadLoadTip);
 		if (itemFooter.isLoading()) {
 			return;
 		}
-		
+
 		final CustomTextView msg = (CustomTextView)v.findViewById(R.id.showthreadMsg);
 		final int postId = Integer.parseInt(item.get("postid").toString());
 		NetClientCallback ncc = new NetClientCallback() {
@@ -374,7 +377,7 @@ public class ShowThreadPage extends Activity implements IXListViewListener, OnIt
 							itemFooter.setLoadFinish();
 							itemFooter.setExpanded();
 						}
-						
+
 					});
 					return;
 				}
@@ -384,12 +387,12 @@ public class ShowThreadPage extends Activity implements IXListViewListener, OnIt
 					public void run() {
 						itemFooter.setLoadFinish();
 					}
-					
+
 				});
 				m_handler.sendEmptyMessage(status);
 			}
 		};
-		
+
 		if (item.containsKey("isExpanded") && item.getInteger("isExpanded") == 1) {
 			item.put("isExpanded", 0);
 			msg.setText((Spannable)item.get("thumbnailSpanned"));
@@ -406,15 +409,25 @@ public class ShowThreadPage extends Activity implements IXListViewListener, OnIt
 			}
 		}
 	}
-	
+
 	private void loadModel(final int page) {
 		Api.getInstance().getForumShowthreadPage(m_id, page, new NetClientCallback() {
 
 			@Override
 			public void execute(int status, String response, List<Cookie> cookies) {
 				if (status == HttpClientUtil.NET_SUCCESS) {
-					JSONObject jsonObj = JSON.parseObject(response);
-					JSONArray jsonArr = jsonObj.getJSONArray("postbits");
+					JSONObject jsonObj;
+					JSONArray jsonArr;
+					try {
+						jsonObj = JSON.parseObject(response);
+						jsonArr = jsonObj.getJSONArray("postbits");
+					} catch (JSONException e) {
+						//Log.v("JSON", response);
+						Looper.prepare();
+						Toast.makeText(ShowThreadPage.this, "JSON数据错误", Toast.LENGTH_SHORT).show();
+						Looper.loop();
+						return;
+					}
 					if (page == 1) {
 						m_totalPage = jsonObj.getInteger("pagenav");
 						m_lastUpdateTime = jsonObj.getLong("time");
@@ -426,16 +439,16 @@ public class ShowThreadPage extends Activity implements IXListViewListener, OnIt
 						}
 						for (int i = 0; i < jsonArr.size(); i++) {
 							m_model.add(jsonArr.getJSONObject(i));
-						}		
+						}
 					}
-				}						
+				}
 				m_handler.sendEmptyMessage(status);
 			}
 		});
 	}
-	
-	
-	private class ShowthreadAdapter extends BaseAdapter {		
+
+
+	private class ShowthreadAdapter extends BaseAdapter {
 
 		public ShowthreadAdapter() {
 			super();
@@ -461,7 +474,7 @@ public class ShowThreadPage extends Activity implements IXListViewListener, OnIt
 			if (convertView == null) {
 				convertView = getLayoutInflater().inflate(R.layout.show_thread_item, null);
 			}
-			
+
 			TextView username = (TextView)convertView.findViewById(R.id.showthreadUsername);
 			TextView floorNum = (TextView)convertView.findViewById(R.id.showThreadFloorNum);
 			floorNum.setText((position + 1) + "#");
@@ -469,7 +482,7 @@ public class ShowThreadPage extends Activity implements IXListViewListener, OnIt
 			final CustomTextView msg = (CustomTextView)convertView.findViewById(R.id.showthreadMsg);
 			ImageViewWithCache img = (ImageViewWithCache)convertView.findViewById(R.id.showthreadHeadImg);
 			ThreadItemFooter itemFooter = (ThreadItemFooter)convertView.findViewById(R.id.showthreadLoadTip);
-			
+
 			final JSONObject item = m_model.getJSONObject(position);
 			username.setText(Html.fromHtml(item.get("username").toString()));
 			posttime.setText(item.get("postdate")+" "+item.get("posttime"));
@@ -483,11 +496,11 @@ public class ShowThreadPage extends Activity implements IXListViewListener, OnIt
 			}else {
 				img.setImageResource(R.drawable.default_user_head_img);
 			}
-			
+
 	        img.setOnClickListener(new View.OnClickListener() {
 	        	@Override
 	        	public void onClick(View v) {
-	        		
+
 	        		if (Api.getInstance().isLogin()) {
 		        		Bundle data = new Bundle();
 		        		data.putInt("user_id", item.getInteger("userid"));
@@ -501,8 +514,8 @@ public class ShowThreadPage extends Activity implements IXListViewListener, OnIt
 	        					Toast.LENGTH_SHORT).show();
 	        		}
 	        	}
-	        }); 
-	        
+	        });
+
 			//格式化缩略帖子缩略信息的Runnable
 			Runnable runFormatMessage = new Runnable() {
 
@@ -520,12 +533,12 @@ public class ShowThreadPage extends Activity implements IXListViewListener, OnIt
 							msg.setMovementMethod(LocalLinkMovementMethod.getInstance());
 							msg.setFocusable(false);
 						}
-						
+
 					});
 				}
-				
+
 			};
-			
+
 
 			//长文章的点击扩展
 			itemFooter.setVisibility((item.getInteger("thumbnail") == 1)?View.VISIBLE:View.GONE);
@@ -541,13 +554,13 @@ public class ShowThreadPage extends Activity implements IXListViewListener, OnIt
 				}
 				itemFooter.setCollapsed();
 			}
-			
+
 			//图片中带附件处理
 			View attachmentView = convertView.findViewById(R.id.showthreadAttachment);
 			if (!item.containsKey("thumbnailattachments") && !item.containsKey("otherattachments")) {
 				attachmentView.setVisibility(View.GONE);
 			}
-			
+
 			LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
 			lp.setMargins(0, 5, 0, 0);
 			LinearLayout attachmentList = (LinearLayout)convertView.findViewById(R.id.showThreadImgAttachmentList);
@@ -556,7 +569,7 @@ public class ShowThreadPage extends Activity implements IXListViewListener, OnIt
 				attachmentView.setVisibility(View.VISIBLE);
 				attachmentList.removeAllViews();
 				for (int i = 0; i < arr.size(); i++) {
-					
+
 					ImageViewWithCache imgWithCache = new ImageViewWithCache(ShowThreadPage.this);
 					imgWithCache.setLayoutParams(lp);
 					int attachmentId = arr.getJSONObject(i).getInteger("attachmentid");
@@ -565,7 +578,7 @@ public class ShowThreadPage extends Activity implements IXListViewListener, OnIt
 						imgWithCache.setImageUrl(url, Api.getInstance().getCookieStorage().getCookies());
 						imgWithCache.setTag(url);
 						imgWithCache.setOnClickListener(new OnClickListener() {
-							
+
 							@Override
 							public void onClick(View v) {
 								 CustomTextView.ThumbnailAttachmentsClick(v.getTag().toString(), v);
@@ -578,21 +591,21 @@ public class ShowThreadPage extends Activity implements IXListViewListener, OnIt
 					attachmentList.addView(imgWithCache);
 				}
 			}
-			
+
 			//其他类型附件处理
 			attachmentList = (LinearLayout)convertView.findViewById(R.id.showThreadOtherAttachmentList);
 			if (item.containsKey("otherattachments")) {
 				JSONArray arr = item.getJSONArray("otherattachments");
 				attachmentView.setVisibility(View.VISIBLE);
 				attachmentList.removeAllViews();
-				
+
 				for (int i = 0; i < arr.size(); i++) {
 					TextView filename = new TextView(ShowThreadPage.this);
 					filename.setLayoutParams(lp);
 					filename.setTextColor(Color.BLUE);
 					filename.setText(arr.getJSONObject(i).getString("filename"));
 					filename.setOnClickListener(new OnClickListener() {
-						
+
 						@Override
 						public void onClick(View v) {
 							Toast.makeText(v.getContext(), "暂不支持附件下载", Toast.LENGTH_SHORT).show();
@@ -602,7 +615,7 @@ public class ShowThreadPage extends Activity implements IXListViewListener, OnIt
 					attachmentList.addView(filename);
 				}
 			}
-			
+
 			return convertView;
 		}
 	}
